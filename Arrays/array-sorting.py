98f0bcaaa5ab4f95bff9eb504fa2f69df473c9ef
Hanjala <hanjalashihab1@gmail.com>
2025-06-03 13:52:54 +0600

    sorting arrays in NumPy

---
+++ Arrays/array-sorting.py
@@ -0,0 +1,26 @@
+import numpy as np
+
+arr = np.array([3, 2, 0, 1])
+
+print(np.sort(arr))   #This method returns a copy of the array, leaving the original array unchanged.
+print()
+
+
+
+arr = np.array(['banana', 'cherry', 'apple'])
+
+print(np.sort(arr))
+print()
+
+
+
+arr = np.array([True, False, True])
+
+print(np.sort(arr))
+print()
+
+
+
+arr = np.array([[3, 2, 4], [5, 0, 1]])
+
+print(np.sort(arr))
