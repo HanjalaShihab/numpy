6d2c987b35257ce638ffe6c2912be9e531b7548b
HanjalaShihab <shihab2305341402@diu.edu.bd>
2025-06-11 13:29:06 +0600

    binomial distributions in NumPy

--- Random/binomial-distribution.py
+++ Random/binomial-distribution.py
@@ -1,3 +1,11 @@
+#Binomial Distribution is a Discrete Distribution.
+
+#It describes the outcome of binary scenarios, e.g. toss of a coin, it will either be head or tails.
+#It has three parameters:
+    #n - number of trials.
+    #p - probability of occurence of each trial (e.g. for toss of a coin 0.5 each).
+    #size - The shape of the returned array.
+
 from numpy import random
 import matplotlib.pyplot as plt
 import seaborn as sns
