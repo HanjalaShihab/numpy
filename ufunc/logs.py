5431505dbcc457f665f07bca35aa4099780744d7
hanjala <hanjalashihab1@gmail.com>
2025-06-25 11:22:04 +0600

    ufunction logs in NumPy

---
+++ ufunc/logs.py
@@ -0,0 +1,19 @@
+import numpy as np
+
+
+# log performing at base 2
+data = np.arange(1,10)
+print(np.log2(data))
+print()
+
+
+# log performing at base 10
+x = np.arange(1,20)
+print(np.log10(x))
+print()
+
+
+
+# log performing at base e or natural log
+y = np.arange(1, 30)
+print(np.log(y))
