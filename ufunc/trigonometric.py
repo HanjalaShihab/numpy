17b4baa90686cc360ab73e729cd36eb5fe8dea2f
HanjalaShihab <shihab2305341402@diu.edu.bd>
2025-07-03 21:26:56 +0600

    Trigonometric in NumPy

--- ufunc/trigonometric.py
+++ ufunc/trigonometric.py
@@ -4,3 +4,11 @@ import numpy as np
 
 x = np.sin(np.pi/2)
 print(x)
+
+
+#Find sine values for all the values in arr:
+
+arr = np.array([np.pi/2, np.pi/3, np.pi/4, np.pi/5])
+
+x = np.sin(arr)
+print(x)
