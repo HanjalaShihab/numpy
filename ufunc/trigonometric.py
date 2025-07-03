7275f44d74356a5d80f34ba4f221a9547e91ea8c
HanjalaShihab <shihab2305341402@diu.edu.bd>
2025-07-03 21:29:30 +0600

    Trigonometric in NumPy

--- ufunc/trigonometric.py
+++ ufunc/trigonometric.py
@@ -4,6 +4,7 @@ import numpy as np
 
 x = np.sin(np.pi/2)
 print(x)
+print()
 
 
 #Find sine values for all the values in arr:
@@ -11,4 +12,11 @@ print(x)
 arr = np.array([np.pi/2, np.pi/3, np.pi/4, np.pi/5])
 
 x = np.sin(arr)
+print(x)
+print()
+
+
+#Convert degrees into radians:
+arr = np.array([90,180, 270, 360])
+x = np.deg2rad(arr)
 print(x)
