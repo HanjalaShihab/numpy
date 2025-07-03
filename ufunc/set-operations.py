a4e4f4b69930697cd63faee06f2e5f8281ad753e
HanjalaShihab <shihab2305341402@diu.edu.bd>
2025-07-03 21:46:26 +0600

    Set operations in NumPy

--- ufunc/set-operations.py
+++ ufunc/set-operations.py
@@ -7,8 +7,12 @@ arr = np.array([1,1,1,2,3,4,5,5,6,7])
 
 x = np.unique(arr)  #it will remove the duplicate values
 print(x)
+print()
 
 
 #Finding union:
 arr1 = np.array([1,2,3,4])
 arr2 = np.array([3,4,5,6])
+
+newarr = np.union1d(arr1,arr2)
+print(newarr)
