f9c27de3fffaa35fc0659d4997e8928905856437
HanjalaShihab <shihab2305341402@diu.edu.bd>
2025-07-03 21:44:48 +0600

    Set operations in NumPy

--- ufunc/set-operations.py
+++ ufunc/set-operations.py
@@ -1,3 +1,11 @@
 #A set in mathematics is a collection of unique elements:
 #Set are used for operations involving frequent intersection, union and difference operations.
 
+import numpy as np
+
+arr = np.array([1,1,1,2,3,4,5,5,6,7])
+
+x = np.unique(arr)  #it will remove the duplicate values
+print(x)
+
+
